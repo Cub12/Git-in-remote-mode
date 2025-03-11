852d4d022e34a09a12ccbbb606406505bad8f95c
Cub12 <seriy.cub@gmail.com>
2025-03-11 16:38:44 +0100

    Resolve the conflict

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -59,10 +59,10 @@ public class Date {
    *
 
    */
-  public Date(int tag, int monat, int jahr) {
-    this.day = tag;
-    this.month = monat;
-    this.year = jahr;
+  public Date(int day, int month, int year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
   }
 
   /**
